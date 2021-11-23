57b1ae80c853cd913233c2da661f0ae1735d7026
RTAcps <rodrigoandrade20@yahoo.com.br>
2021-11-23 09:02:50 -0300

    Inicia o React && Cria o Header

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles/global.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>AcessaBR Campinas</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/global.scss";
+import Header from "./components/Header";
+
+ReactDOM.render(
+  <div className="main__wrapper">
+    <div className="main__container">
+      <Header />
+    </div>
+  </div>,
+  document.getElementById("root")
+);
