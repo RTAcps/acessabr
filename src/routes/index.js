165d8c28740fb71dd0b61f5f5f1c414f50426e16
RTAcps <rodrigoandrade20@yahoo.com.br>
2021-11-26 07:29:04 -0300

    Adiciona route

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+} from "react-router-dom";
+import Home from "../pages/Home";
+
+const Routes = (props) => {
+  return (
+    <Router>
+      <Switch>
+        {/* <Route path="/:state/:city" element={<Home />} /> */}
+        <Route path="/" element={<Home city={props.city} />} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
